a5e01598c01510efe8cec9a220d9c71fa36322b1
etnom <montgomery.okdaily@gmail.com>
2018-04-17 22:40:26 -0700

    function to deal with pot input

--- V1/V1.ino
+++ V1/V1.ino
@@ -14,71 +14,74 @@ By Monty C, 04/15/18
 
 
 //libraries to include
-#include <Adafruit_SSD1306.h>											//import library for display driver
-#include <Adafruit_GFX.h>												//import library for display graphics
-#include <Button.h>														//import library for buttons
+#include <Adafruit_SSD1306.h>												//import library for display driver
+#include <Adafruit_GFX.h>													//import library for display graphics
+#include <Button.h>															//import library for buttons
 
 //analog input pins
-#define IR_REC_PIN 0													//pin for IR receiver                  	
-#define VOLTMETER_PIN 1													//pin for voltmeter
-#define POT_PIN 3														//pin for potentiometer
+#define IR_REC_PIN 0														//pin for IR receiver                  	
+#define VOLTMETER_PIN 1														//pin for voltmeter
+#define POT_PIN 3															//pin for potentiometer
 
 //button output pins
-#define TRIGGER_BTN_PIN 4												//pin for trigger button
-#define RELOAD_BTN_PIN 7												//pin for reload switch
-#define MAG_SZ_TOG_BTN_PIN 8											//pin for magazine size toggle button
+#define TRIGGER_BTN_PIN 4													//pin for trigger button
+#define RELOAD_BTN_PIN 7													//pin for reload switch
+#define MAG_SZ_TOG_BTN_PIN 8												//pin for magazine size toggle button
 
 //PWM output pins
-#define MOTOR_OUT_PIN 11												//pin for PWM out to control motor
+#define MOTOR_OUT_PIN 11													//pin for PWM out to control motor
 
 //macros for display functionality
-#define SCREEN_WIDTH 128												//screen width of display
-#define SCREEN_HEIGHT 64												//screen height of display
-#define OLED_RESET 4													//OLED Reset pin
+#define SCREEN_WIDTH 128													//screen width of display
+#define SCREEN_HEIGHT 64													//screen height of display
+#define OLED_RESET 4														//OLED Reset pin
 
 //macros for IR gate and chrono functionality
-#define IR_GATE_TRIP_VAL 60               								//value at which the IR gate is considered "blocked", or "tripped"
-#define DART_LEGTH 0.23622												//length of dart, in feet
+#define IR_GATE_TRIP_VAL 60               									//value at which the IR gate is considered "blocked", or "tripped"
+#define DART_LEGTH 0.23622													//length of dart, in feet
 
 //macros for voltmeter functionality
-#define R1 100000f														//reistance of R1 to calculate voltage division
-#define R2 10000f														//reistance of R2 to calculate voltage division
+#define R1 100000f															//reistance of R1 to calculate voltage division
+#define R2 10000f															//reistance of R2 to calculate voltage division
 
 //macros for button functionality
-#define PU_ENABLE false													//don't enable internal pullup resistors for buttons
-#define INVERT false													//don't enable invert button readings
-#define DB_TIME 25														//debouce time for button
+#define PU_ENABLE false														//don't enable internal pullup resistors for buttons
+#define INVERT false														//don't enable invert button readings
+#define DB_TIME 25															//debouce time for button
 
-Adafruit_SSD1306 display(OLED_RESET);									//display object instantiated from SSD1306 driver library
+Adafruit_SSD1306 display(OLED_RESET);										//display object instantiated from SSD1306 driver library
 
-Button triggerBtn (TRIGGER_BTN_PIN, PU_ENABLE, INVERT, DB_TIME);		//Trigger button, from Button lib
-Button reloadBtn (RELOAD_BTN_PIN, PU_ENABLE, INVERT, DB_TIME);			//Reload button, from Button lib
-Button magSzTogBtn (MAG_SZ_TOG_BTN_PIN, PU_ENABLE, INVERT, DB_TIME);	//Magazine size toggle button, from Button lib
+Button triggerBtn (TRIGGER_BTN_PIN, PU_ENABLE, INVERT, DB_TIME);			//Trigger button, from Button lib
+Button reloadBtn (RELOAD_BTN_PIN, PU_ENABLE, INVERT, DB_TIME);				//Reload button, from Button lib
+Button magSzTogBtn (MAG_SZ_TOG_BTN_PIN, PU_ENABLE, INVERT, DB_TIME);		//Magazine size toggle button, from Button lib
 
-uint8_t magSizeArr[] = {5, 6, 10, 12, 15, 18, 20, 22, 25, 36, 0};  		//keep track of the magazine sizes
-uint8_t currentMagSize = 0;  											//keep track of the current magazine size
-uint8_t currentAmmo = magSizeArr[currentMagSize];    					//keep track of how much ammo there currently is
-uint8_t maxAmmo = magSizeArr[currentMagSize];    						//keep track of what the max ammo is, for use when reloading 
+uint8_t magSizeArr[] = {5, 6, 10, 12, 15, 18, 20, 22, 25, 36, 0};  			//keep track of the magazine sizes
+uint8_t currentMagSize = 0;  												//keep track of the current magazine size
+uint8_t currentAmmo = magSizeArr[currentMagSize];    						//keep track of how much ammo there currently is
+uint8_t maxAmmo = magSizeArr[currentMagSize];    							//keep track of what the max ammo is, for use when reloading 
 
-float chronoReading = 123;												//keep track of chrono readings
+float chronoReading = 123;													//keep track of chrono readings
 
-float voltage = 0;														//keep track of voltage from voltmeter
-float lastVoltage = 0;													//keep track of last voltage reading
+float voltage = 0;															//keep track of voltage from voltmeter
+float lastVoltage = 0;														//keep track of last voltage reading
 
-int motorVel = 0;														//keep track of motor velocity via PWM
+uint8_t motorVel = 0;														//keep track of motor velocity via PWM
+uint8_t lastMotorVel = 0;													//keep track of last motor velocity
+double lastMotorVelCheckTime = 0;											//keep track of time for debouncing
 
-uint8_t toUpdateDisplay = true;											//flag to update display. Don't want to update display every loop()	 
+uint8_t toUpdateDisplay = true;												//flag to update display. Don't want to update display every loop()	 
 
 
 void setup() {
-	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);							//begin display with correct I2C address
-	display.clearDisplay();												//clear display of any jumk that might be on it
-	updateDisplay();													//update display to print default values
+	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);								//begin display with correct I2C address
+	display.clearDisplay();													//clear display of any jumk that might be on it
+	updateDisplay();														//update display to print default values
 }
 
 void loop() {
 	changeMagSizes();														//change magazine sizes, if needed
 	reload();																//reload, if needed
+	potInput();																//deal with potentieometer as input
 	updateDisplay();														//update display, if needed
 }
 
@@ -124,7 +127,7 @@ void updateDisplay() {														//function to deal with updating display
 
 }
 
-void changeMagSizes () {													//function to deal with changing magazine sizes 
+void changeMagSizes() {														//function to deal with changing magazine sizes 
 	magSzTogBtn.read();														//read the magazine size toggle button's value
 	if (magSzTogBtn.wasPressed()) {											//if mag size toggle btn pressed
 	    currentMagSize = (currentMagSize < 									//cycle through mag sizes based on array
@@ -138,12 +141,25 @@ void changeMagSizes () {													//function to deal with changing magazine s
 	}	
 }
 
-void reload () {															//function to deal with reloading 
+void reload() {																//function to deal with reloading 
 	reloadBtn.read();														//read the value of the switch within the magwell
 	if (reloadBtn.wasPressed()) {											//if switch in magwell pressed
 		currentAmmo = maxAmmo;												//reset magazine size to max
 
 	    toUpdateDisplay = true;												//data has been changed, update display to show data
+	}
+}
+
+void potInput() {															//function to deal with pot input changes			
+	if ( ((millis() + DB_TIME) > lastMotorVelCheckTime) && 					//make sure to check at certian intervals instead of every loop()
+		(motorVel != lastMotorVel) ) {										//make sure motor velocity changed
+
+		lastMotorVel = motorVel = (analogRead(POT_PIN)/4) - 1;				//set new motor velocity
+		lastMotorVelCheckTime = millis();									//reset time for checking motor vel
+
+	    toUpdateDisplay = true;												//data has been changed, update display to show data
+	} else {																//if motor velocity didn't change or not time to check
+		lastMotorVel = (analogRead(POT_PIN)/4) - 1;							//set new last motor velocity to compare changes
 	}
 }
 
